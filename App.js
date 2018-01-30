82cd35adb456de9f9337c7ebafaef7da9e119d49
Saad <saadbennmushtaq@gmail.com>
2018-01-29 20:04:54 -0600

    Crypto currency structure

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
+
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = () => {
+	return (
+
+		<View> 
+			<Text>Cryptocurrency App</Text> 
+		</View>
+	)
+}
+
+
+const styles = StyleSheet.create({
+	headerContainer: { 
+		display: 'flex',
+		marginTop: 55,
+		alignItems:'center',
+	},
+	header: {
+		fontWeight: "bold",
+		fontSize: 20,
+	}
+})
+
+const { headerContainer, header } = styles;
+
+
+export default Header;

852ad4f89a8cf078451ad8bf747d206cc9b5b82e
Saad <saadbennmushtaq@gmail.com>
2018-02-28 21:32:40 -0600

    Set up reducers

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import RootReducer from './Reducers';
+import { Platform } from 'react-native';
+import devTools from 'remote-redux-devtools';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 const Store = createStore(RootReducer)

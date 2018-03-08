dbdb4f9530649f01138c42953dde71a266351236
Saad <saadbennmushtaq@gmail.com>
2018-03-07 20:11:24 -0600

    Set up Store

--- src/Store.js
+++ src/Store.js
@@ -4,5 +4,17 @@ import { Platform } from 'react-native';
 import devTools from 'remote-redux-devtools';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import RootReducer from './Reducers';
 
-const Store = createStore(RootReducer)
+
+const Store = createStore(RootReducer, 
+	compose(
+		middleware,
+		devTools({
+			name: Platform.OS,
+			hostname: 'localhost',
+			port: 5678
+		}),
+	)
+);

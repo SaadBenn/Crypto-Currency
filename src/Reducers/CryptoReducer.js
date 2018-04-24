b35f07fecca4c0c91ad78f8201dfb45042e09037
Saad <saadbennmushtaq@gmail.com>
2018-04-23 21:06:30 -0500

    Importing the action types and using switch conditional.

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,46 @@
+import {
+	FETCHING_DATA,
+	FETCHING_DATA_SUCCESS,
+	FETCHING_DATA_FAIL
+} from './../Actions/ActionTypes';
+
+const initialState = {
+	isFetching: null,
+	data: [],
+	hasError: false,
+	errorMessage: null,
+};
+
 export default function(state = [], action) {
-	return state;
+
+	switch(action.type) {
+
+		case FETCHING_DATA:
+			return Object.assign({}, state, {
+				isFetching: true,
+				data: null,
+				hasError: false,
+				errorMessage: null
+		});
+
+		case FETCHING_DATA_SUCCESS:
+			return Object.assign({}, state, {
+				isFetching: false,
+				data: action.payload,
+				hasError: false,
+				errorMessage: null
+		});
+
+		case FETCHING_DATA_FAIL:
+			return Object.assign({}, state, {
+				isFetching: false,
+				data: action.payload,
+				hasError: true,
+				errorMessage: action.err
+		});
+			
+	default:		
+		return state;
+	}
+	
 }

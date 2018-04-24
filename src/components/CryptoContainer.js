4df5fb4d17a7741f3c03dc7c6fc4329c9b390715
Saad <saadbennmushtaq@gmail.com>
2018-04-23 21:28:27 -0500

    Call the FetchData action.

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import FetchData from './../Actions/FetchData';
 
 class CryptoContainer extends Component {
+	ComponentDidMount() {
+		this.props.FetchData();
+	}
+
 	render() {
 		return (
 			<View>
@@ -18,4 +23,4 @@ function mapStateToProps(state) {
 	}
 }
 
-export default connect(mapStateToProps)(CryptoContainer)
+export default connect(mapStateToProps, {FetchData})(CryptoContainer)

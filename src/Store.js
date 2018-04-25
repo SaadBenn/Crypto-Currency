8ae34bed874f83c1a86286df82c83da766e3ef6f
Saad <saadbennmushtaq@gmail.com>
2018-04-24 20:15:14 -0500

    Add whiteline for code readability.

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import RootReducer from './Reducers';
 import { Platform } from 'react-native';
+
 import devTools from 'remote-redux-devtools';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';

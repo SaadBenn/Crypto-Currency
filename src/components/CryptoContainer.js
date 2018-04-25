1566802b4de62f023bd4e4ef4eeca4a4d2825102
Saad <saadbennmushtaq@gmail.com>
2018-04-24 20:14:48 -0500

    Iterate over crypto data and return a coin (statless component) object.

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import FetchData from './../Actions/FetchData';
+import Coin from './Coin';
 
 class CryptoContainer extends Component {
+	
 	ComponentDidMount() {
 		this.props.FetchData();
 	}
 
+	renderCoin() {
+		const { crypto } = this.props;
+		console.log(crypto)
+
+		return crypto.data.map((coin, index)) =>
+			<Coin
+				key={index}
+				coin_name={coin_name}
+				symbol={coin.symbol}
+				price_usd={coin.price_usd}
+				percent_change_24h={coin.percent_change_24h}
+				percent_change_7d={coin.percent_change_7d}
+			/>
+		)	
+	}
+
 	render() {
+		const { crypto } = this.props;
 		return (
 			<View>
-				<Text> Container </Text>
+				{this.renderCoin()}
 			</View>
 		)
 	}
